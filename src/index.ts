26fbd79855695410da2fea2f2e84da0c28f5518c
Nikola <nsretkovic@elfak.rs>
2021-07-14 02:57:06 +0200

    entry final

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,19 @@
-import { drawPlayer, drawTeamCreator } from "./view/drawingFunctions";
-import { Player } from "./models/player";
-import { playerInputObs } from "./controllers/observables";
+import {
+  createElements,
+  drawResults,
+  drawTeamCreator,
+} from "./view/drawingFunctions";
+import { makeTeamObs } from "./controllers/observables";
 
 let inputFields: HTMLInputElement[] = [];
 let playerDetails: HTMLDivElement[] = [];
 let playerNameLabels: HTMLLabelElement[] = [];
 let playerStatsLabels: HTMLLabelElement[] = [];
-for (let i = 0; i < 5; ++i) {
-  inputFields[i] = document.createElement("input");
-  playerDetails[i] = document.createElement("div");
-  playerDetails[i].classList.add("playerDetails");
-  playerNameLabels[i] = document.createElement("label");
-  playerStatsLabels[i] = document.createElement("label");
-}
 
-const teamViewContainer: HTMLDivElement = document.createElement("div");
-const midDiv = document.createElement("div");
-midDiv.classList.add("midDiv");
+let teamViewContainer: HTMLDivElement = document.createElement("div");
+
+let midDiv: HTMLDivElement;
+
 let positionDivs: HTMLDivElement[] = [];
 let classNames: string[] = [
   "gkDiv",
@@ -26,70 +23,26 @@ let classNames: string[] = [
   "attDiv",
 ];
 
-for (let i = 0; i < 5; ++i) {
-  positionDivs[i] = document.createElement("div");
-  positionDivs[i].classList.add(classNames[i]);
-  if (classNames[i] !== "leftMidDiv" || classNames[i] !== "rightMidDiv")
-    teamViewContainer.appendChild(positionDivs[i]);
-  if (i === 2) teamViewContainer.appendChild(midDiv);
-}
-
-midDiv.appendChild(positionDivs[2]);
-midDiv.appendChild(positionDivs[3]);
+createElements(
+  inputFields,
+  playerDetails,
+  playerNameLabels,
+  playerStatsLabels,
+  teamViewContainer,
+  midDiv,
+  positionDivs,
+  classNames
+);
 
 drawTeamCreator(document.body, teamViewContainer, inputFields);
-
-playerInputObs(inputFields[0], positionDivs[0], "goalkeepers").subscribe(
-  (player: Player) =>
-    drawPlayer(
-      positionDivs[0],
-      player,
-      playerDetails[0],
-      playerNameLabels[0],
-      playerStatsLabels[0]
-    )
+makeTeamObs(
+  inputFields,
+  positionDivs,
+  playerDetails,
+  playerNameLabels,
+  playerStatsLabels
 );
 
-playerInputObs(inputFields[1], positionDivs[1], "defenders").subscribe(
-  (player: Player) =>
-    drawPlayer(
-      positionDivs[1],
-      player,
-      playerDetails[1],
-      playerNameLabels[1],
-      playerStatsLabels[1]
-    )
-);
-
-playerInputObs(inputFields[2], positionDivs[2], "leftmids").subscribe(
-  (player: Player) =>
-    drawPlayer(
-      positionDivs[2],
-      player,
-      playerDetails[2],
-      playerNameLabels[2],
-      playerStatsLabels[2]
-    )
-);
-
-playerInputObs(inputFields[3], positionDivs[3], "rightmids").subscribe(
-  (player: Player) =>
-    drawPlayer(
-      positionDivs[3],
-      player,
-      playerDetails[3],
-      playerNameLabels[3],
-      playerStatsLabels[3]
-    )
-);
-
-playerInputObs(inputFields[4], positionDivs[4], "attackers").subscribe(
-  (player: Player) =>
-    drawPlayer(
-      positionDivs[4],
-      player,
-      playerDetails[4],
-      playerNameLabels[4],
-      playerStatsLabels[4]
-    )
-);
+const resultsDiv = document.createElement("div");
+resultsDiv.classList.add("resultsDiv");
+document.body.appendChild(resultsDiv);
